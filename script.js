016e08fd4dab5976bcc3091efa53d701dba25ef9
LIVY DJ <liss.cruz8@gmail.com>
2023-07-26 17:27:50 -0500

    Cajero Automatico

---
+++ script.js
@@ -0,0 +1,25 @@
+const cuerpodocumento = document.body
+
+var encabezado = document.getElementById('headeriniciologin')
+var scConsulta = document.getElementById('scConsulta')
+var scIngreso = document.getElementById('scIngreso')
+var scRetiro = document.getElementById('scRetiro')
+var nav = document.getElementById('menuprincipal')
+
+function inicio(){
+    encabezado.hidden = false;
+    scConsulta.hidden = true;
+    scIngreso.hidden = true;
+    sc
+}
+
+function Login(){
+    encabezado.hidden = true
+}
+
+
+
+function Consultar(){
+    var div = document.getElementById('dvprueba')
+    div.innerHTML = 'puebaok'
+}
